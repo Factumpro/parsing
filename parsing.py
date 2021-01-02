905c7bb916bdfadacdb32ab80bd3d6b4df7c6d5a
Factumpro <slidzonaoleg@gmail.com>
2021-01-02 04:20:05 +0100

    def new function/ edit const varialbe, edit function get_html

--- parsing.py
+++ parsing.py
@@ -3,14 +3,17 @@ import csv
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-HOST = 'https://www.enzomuratore.com/'
-REQUEST_URL = 'https://www.enzomuratore.com/sklep/'
-
 STATUS_CODE_ACCEPT = 200
-CSV_FILE = 'parsing-file.csv'
+REQUEST_URL = 'https://www.enzomuratore.com/sklep/'
+CSV_FILE = 'csv/parsing-file.csv'
+NEED_INFO = (
+    'title',
+    'available',
+    'check-link'
+)
 
 
-def fake_user_agent():
+def fake_useragent():
     user_agent = {
         'user-agent': UserAgent().random
     }
@@ -18,15 +21,20 @@ def fake_user_agent():
 
 
 def get_html(url, params=''):
-    user_agent = fake_user_agent()
-    requests_variable = requests.get(url, headers=user_agent, params=params)
+    useragent_update = fake_useragent()
+    requests_variable = requests.get(url, headers=useragent_update, params=params)
+    if requests_variable.status_code == STATUS_CODE_ACCEPT:
+        requests_variable = requests_variable.text
+        pass
+    else:
+        print('ERROR')
 
     return requests_variable
 
 
 def get_content(html):
-    soup_variable = BeautifulSoup(html, 'html.parser')
     products_variable = []
+    soup_variable = BeautifulSoup(html, 'html.parser')
     search_variable = soup_variable.find_all('li', class_='product')
 
     for item in search_variable:
@@ -37,13 +45,14 @@ def get_content(html):
                 'check-link': item.find('a').get('href')
             }
         )
+
     return products_variable
 
 
 def save_csv(items, path):
     with open(path, 'w', newline='', encoding='UTF-8') as file:
         writer = csv.writer(file, delimiter=';')
-        writer.writerow(['title', 'available', 'check-link'])
+        writer.writerow(NEED_INFO)
 
         for item in items:
             writer.writerow([item['title'], item['available'], item['check-link']])
@@ -51,14 +60,9 @@ def save_csv(items, path):
 
 def main():
     site = get_html(REQUEST_URL)
-
-    if site.status_code == STATUS_CODE_ACCEPT:
-        products = []
-        products.extend(get_content(site.text))
-        save_csv(products, CSV_FILE)
-        pass
-    else:
-        print('ERROR')
+    products = []
+    products.extend(get_content(site))
+    save_csv(products, CSV_FILE)
 
 
 main()
